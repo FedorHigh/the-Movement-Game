b7780966c68bc9bf166bd5f1cb33a2513e9e11fe
Dmitry <85012558+DDmitry55@users.noreply.github.com>
2025-07-03 15:36:45 +0300

    canvas changed

--- [ORIGINAL] MovementGame/Assets/LevelManager.cs
+++ [ORIGINAL] MovementGame/Assets/LevelManager.cs
@@ -2,7 +2,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject PanelPause;
+
+
+    public bool isPaused; // Flag to track pause state
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Toggle pause state on Escape key press
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        // Set Time.timeScale to 0 to pause gameplay
+        Time.timeScale = 0;
+        // Make PauseMenu panel visible (activate its gameObject)
+        PanelPause.gameObject.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        // Set Time.timeScale back to 1 to resume gameplay
+        Time.timeScale = 1;
+        // Hide PauseMenu panel (deactivate its gameObject)
+        PanelPause.gameObject.SetActive(false);
+    }
     public void OnBossDeath() {
         Debug.Log("BOSS IS DEAD");
         SceneManager.LoadScene("Win");
